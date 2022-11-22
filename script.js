1598c991ce660c7e8dcc868fd4122fa0fb61726e
khylle02 <74064901+khylle02@users.noreply.github.com>
2022-11-22 16:13:57 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let generateButton = document.getElementById('gen-pass-btn');
+let passwordEl1 = document.getElementById('password-one');
+let passwordEl2 = document.getElementById('password-two');
+
+
+generateButton.addEventListener("click", function() {
+    passwordEl1.textContent = " "
+    for (let i = 0; i < 15; i++) {
+        let passwordIndexOne = Math.floor(Math.random() * characters.length)
+        passwordEl1.textContent += characters[passwordIndexOne]
+    }
+    passwordEl2.textContent = " "
+    for (let i = 0; i < 15; i++) {
+        let passwordIndexTwo = Math.floor(Math.random() * characters.length)
+        passwordEl2.textContent += characters[passwordIndexTwo]
+    }
+})
